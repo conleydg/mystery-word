de386ce030a18f0072c2a1b00a4aa1cb4a9c50c0
David Conley <David@Davids-MacBook-Pro.local>
2016-03-31 17:33:45 -0400

    Updating files before the nd of the day

--- mystery_word.py
+++ mystery_word.py
@@ -14,7 +14,7 @@ def read_words_file(easy_words, normal_words, hard_words):
             elif len(word) >= 8:
                 hard_words.append(word)
 
-def game_difficulty(user_difficulty_input, easy_words, normal_words, hard_words):
+def chose_word_difficulty_based(user_difficulty_input, easy_words, normal_words, hard_words):
     if user_difficulty_input == 'easy':
         return random.choice(easy_words)
     elif user_difficulty_input == 'normal':
@@ -22,11 +22,42 @@ def game_difficulty(user_difficulty_input, easy_words, normal_words, hard_words)
     elif user_difficulty_input == 'hard':
         return random.choice(hard_words)
 
+def guess_hit_or_miss(user_input, computers_word, correct_guesses, incorrect_guesses):
+    if user_input in computers_word:
+        return True
+    else:
+        return False
+
+
+
+def user_guess():
+    while True:
+        user_input = input('Please enter your guess: ')
+        if len(user_input) != 1 or  user_input.isdigit():
+            print('Try again - Only enter one letter!!')
+        else:
+            break
+    return user_input
+
+def word_dispay(computers_word,correct_guesses):
+    for letter in computers_word:
+        if letter in correct_guesses:
+            print(letter, " ", end="")
+        else:
+            print('- ', end="")
+
+
+
 
 def main():
     easy_words = []
     normal_words = []
     hard_words = []
+    correct_guesses = []
+    incorrect_guesses = []
+
+    guess_count = 0
+
     read_words_file(easy_words, normal_words, hard_words)
 
     welcome_print = print('Welcome to the mystery word game!')
@@ -34,12 +65,38 @@ def main():
     user_difficulty_input = input('Which mode you would you like to Play?'
                         'Please type Easy/Normal/Hard or E/N/H and hit enter:  ').lower()
 
-    difficulty = game_difficulty(user_difficulty_input, easy_words, normal_words, hard_words)
+    computers_word = chose_word_difficulty_based(user_difficulty_input, easy_words, normal_words, hard_words)
 
-    print(difficulty)
-    print(len(easy_words))
-    print(len(normal_words))
-    print(len(hard_words))
+    print('The computer\'s word contains {} letters'.format(len(computers_word)))
+
+    while guess_count < 8:
+
+        user_input = user_guess()
+
+        if guess_hit_or_miss(user_input, computers_word, correct_guesses, incorrect_guesses):
+                correct_guesses.append(user_input)
+                print('Good Guess!!')
+                correct_guesses.append(user_input)
+        elif not guess_hit_or_miss(user_input, computers_word, correct_guesses, incorrect_guesses):
+                guess_count += 1
+                print('Bad Guess!')
+
+
+        guess_hit_or_miss(user_input, computers_word, correct_guesses, incorrect_guesses)
+
+        word_display = word_dispay(computers_word,correct_guesses)
+
+
+
+
+        remaining_guesses = 8 - guess_count
+
+        print('You have {} guess remaining'.format(remaining_guesses))
+        print(computers_word)
+
+
+
+
 
 
 if __name__ == "__main__":
---
+++ mystery_word_test.py
@@ -0,0 +1,18 @@
+import unittest
+import my_module
+
+class Myster_word(unittest.TestCase):
+
+    # def test_assert_true(self):
+    #     self.assertTrue(my_module.is_true())
+   #
+   # def test_assert_equal(self):
+   #      self.assertEqual(my_module.get_one(), 1)
+
+   
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
